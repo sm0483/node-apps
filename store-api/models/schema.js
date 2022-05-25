a37da62e4d436abf8e6bf6b4c1e7f56fdecd9acd
Sreerag <msratofficial@gmail.com>
2022-05-25 22:19:10 +0530

    feat: add basic schema for db

---
+++ store-api/models/schema.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Name of product must be specified']
+    },
+    price:{
+        type:Number,
+        required:[true,'Price must be specified']
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    company:{
+        type:String,
+        enum:{
+            values:['ikea','liddy','caressa','marcos'],
+            message:`{VALUE} is not supported`
+        }
+    }
+})
+
+
+module.exports=mongoose.model('Product',productSchema);
