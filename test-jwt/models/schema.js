06116dfe620c63f764a9b987cd8ce7e3b02c92c7
Sreerag <msratofficial@gmail.com>
2022-06-08 18:57:04 +0530

    feat: add basic data schema

---
+++ test-jwt/models/schema.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Name of product must be specified']
+    },
+    price:{
+        type:Number,
+        required:[true,'Price must be specified']
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    company:{
+        type:String,
+        enum:{
+            values:['ikea','liddy','caressa','marcos'],
+            message:`{VALUE} is not supported`
+        }
+    }
+
+});
+const Product=mongoose.model('Product',productSchema);
+
+module.exports=Product;
