987b4b78435f40b71b8b7a04bf4a98d9e85c4578
Sreerag <msratofficial@gmail.com>
2022-06-08 18:41:34 +0530

    feat: add basic db set up

--- test-jwt/app.js
+++ test-jwt/app.js
@@ -1,7 +1,24 @@
 const express=require('express');
 const app=express();
 
+const connectDb=require('./db/connect'); //used to connect db
+require('dotenv').config(); //configure env files
 
-app.listen(3000,(req,res)=>{
+
+
+app.listen(3000,(req,res)=>{  //-->used to start server in port 3000
     console.log('server running------');
 })
+
+
+const start=async()=>{
+    try{
+        let connect=await connectDb(process.env.URL);
+        console.log('db connected---');
+    }
+    catch(err){
+        console.trace(err);
+    }
+}
+
+start();
---
+++ test-jwt/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+const connectDb=(url)=>{
+    return mongoose.connect(url,{
+        useUnifiedTopology: true,
+        useNewUrlParser: true,  
+    })
+
+}
+
+
+module.exports=connectDb;
