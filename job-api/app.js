d446311204982945f0e4be0b1bfc5ca4258c21cd
Sreerag <msratofficial@gmail.com>
2022-06-11 11:54:56 +0530

    feat: add mongo db connection

--- job-api/app.js
+++ job-api/app.js
@@ -1,12 +1,26 @@
 const express=require('express');
 const app=express();
 const jobRoute=require('./routes/route');
-
+const connectDb=require('./db/connect');
+require('dotenv').config();
 app.use(express.json());
 
 
 app.listen(3000,()=>{
     console.log('sever started----------');
 })
+
+const start=async()=>{ //-->used to connect with db
+    try{
+        const connect=await connectDb(process.env.URL);
+        console.log('connectd with db----');
+    }
+    catch(err){
+        console.trace(err);
+
+    }
+}
+
+start();
 
 app.use('/api/v1/jobs',jobRoute);
---
+++ job-api/db/connect.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+
+
+const connectDb=(url)=>{
+    return mongoose.connect(url,{
+        useUnifiedTopology: true,
+        useNewUrlParser: true,   
+    })
+}
+
+module.exports=connectDb;
